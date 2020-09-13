ee3faa1abd279cbe6ebdc4f7aebe4ca9bc3cce36
ue-sho <tssu45@gmail.com>
2020-09-13 11:24:32 +0900

    売買APIを作成

--- main.go
+++ main.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"fmt"
-	"time"
 
 	"github.com/ue-sho/trading_system/bitflyer"
 	"github.com/ue-sho/trading_system/config"
@@ -13,14 +12,38 @@ func main() {
 	utils.LoggingSettings(config.Config.LogFile)
 	apiClient := bitflyer.New(config.Config.ApiKey, config.Config.ApiSecret)
 
-	tickerChannel := make(chan bitflyer.Ticker)
-	go apiClient.GetRealTimeTicker(config.Config.ProductCode, tickerChannel)
-	for ticker := range tickerChannel {
-		fmt.Println(ticker)
-		fmt.Println(ticker.GetMidPrice())
-		fmt.Println(ticker.DateTime())
-		fmt.Println(ticker.TruncateDateTime(time.Second))
-		fmt.Println(ticker.TruncateDateTime(time.Minute))
-		fmt.Println(ticker.TruncateDateTime(time.Hour))
+	/* リアルタイムで情報を取得 */
+	// tickerChannel := make(chan bitflyer.Ticker)
+	// go apiClient.GetRealTimeTicker(config.Config.ProductCode, tickerChannel)
+	// for ticker := range tickerChannel {
+	// 	fmt.Println(ticker)
+	// 	fmt.Println(ticker.GetMidPrice())
+	// 	fmt.Println(ticker.DateTime())
+	// 	fmt.Println(ticker.TruncateDateTime(time.Second))
+	// 	fmt.Println(ticker.TruncateDateTime(time.Minute))
+	// 	fmt.Println(ticker.TruncateDateTime(time.Hour))
+	// }
+
+	/* オーダー情報 */
+	order := &bitflyer.Order{
+		ProductCode:     config.Config.ProductCode,
+		ChildOrderType:  "LIMIT", // or MARKET
+		Side:            "BUY",   // or SELL
+		Price:           7000,
+		Size:            0.01,
+		MinuteToExpires: 1,
+		TimeInForce:     "GTC", // キャンセルするまで有効
 	}
+	/* オーダーを出す */
+	res, _ := apiClient.SendOrder(order)
+	fmt.Println(res.ChildOrderAcceptanceID)
+
+	/* オーダー情報をリストをみる */
+	//i := "JRF20181012-144016-140584"
+	//params := map[string]string{
+	//	"product_code": config.Config.ProductCode,
+	//	"child_order_acceptance_id": i,
+	//}
+	//r, _ := apiClient.ListOrder(params)
+	//fmt.Println(r)
 }
