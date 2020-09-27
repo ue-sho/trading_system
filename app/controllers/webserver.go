703c57279a1f8bb0918af28e899ca6ed99f25cd5
ue-sho <tssu45@gmail.com>
2020-09-27 17:40:19 +0900

    ボリンジャーバンドのバックテストを追加

--- app/controllers/webserver.go
+++ app/controllers/webserver.go
@@ -202,10 +202,18 @@ func apiCandleHandler(w http.ResponseWriter, r *http.Request) {
 	events := r.URL.Query().Get("events")
 	if events != "" {
 		if config.Config.BackTest {
-			p, p1, p2 := df.OptimizeEma()
-			log.Println(p, p1, p2)
-			if p > 0 {
-				df.Events = df.BackTestEma(p1, p2)
+			/* EMAのバックテスト */
+			// performance, p1, p2 := df.OptimizeEma()
+			// log.Println(performance, p1, p2)
+			// if performance > 0 {
+			// 	df.Events = df.BackTestEma(p1, p2)
+			// }
+
+			/* ボリンジャーバンドのバックテスト */
+			performance, p1, p2 := df.OptimizeBb()
+			log.Println(performance, p1, p2)
+			if performance > 0 {
+				df.Events = df.BackTestBb(p1, p2)
 			}
 		} else {
 			firstTime := df.Candles[0].Time // 84行めで指定した個数の頭の時間
