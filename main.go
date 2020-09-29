a867b0c071cbd8550ce346909e1698a5d40f4769
ue-sho <tssu45@gmail.com>
2020-09-29 20:08:45 +0900

    自動売買できる機能を追加

--- main.go
+++ main.go
@@ -9,63 +9,69 @@ import (
 )
 
 func main() {
-
-	/* ログの設定 */
 	utils.LoggingSettings(config.Config.LogFile)
-	// apiClient := bitflyer.New(config.Config.ApiKey, config.Config.ApiSecret)
-
-	/* リアルタイムで情報を取得 */
-	// tickerChannel := make(chan bitflyer.Ticker)
-	// go apiClient.GetRealTimeTicker(config.Config.ProductCode, tickerChannel)
-	// for ticker := range tickerChannel {
-	// 	fmt.Println(ticker)
-	// 	fmt.Println(ticker.GetMidPrice())
-	// 	fmt.Println(ticker.DateTime())
-	// 	fmt.Println(ticker.TruncateDateTime(time.Second))
-	// 	fmt.Println(ticker.TruncateDateTime(time.Minute))
-	// 	fmt.Println(ticker.TruncateDateTime(time.Hour))
-	// }
-
-	/* オーダー情報 */
-	// order := &bitflyer.Order{
-	// 	ProductCode:     config.Config.ProductCode,
-	// 	ChildOrderType:  "LIMIT", // or MARKET
-	// 	Side:            "BUY",   // or SELL
-	// 	Price:           7000,
-	// 	Size:            0.01,
-	// 	MinuteToExpires: 1,
-	// 	TimeInForce:     "GTC", // キャンセルするまで有効
-	// }
-
-	/* オーダーを出す */
-	// res, _ := apiClient.SendOrder(order)
-	// fmt.Println(res.ChildOrderAcceptanceID)
-
-	/* オーダー情報をリストをみる */
-	// i := "JRF20181012-144016-140584"
-	// params := map[string]string{
-	//	"product_code": config.Config.ProductCode,
-	//	"child_order_acceptance_id": i,
-	// }
-	// r, _ := apiClient.ListOrder(params)
-	// fmt.Println(r)
-
-	// テーブルを出力する
-	// fmt.Println(models.DbConnection)
-
-	/* リアルタイムにBitFlyerからデータを取ってきてデータベースに保存する */
 	controllers.StreamIngestionData()
 	log.Println(controllers.StartWebServer())
-
-	/* BUYとSELLのシグナルを出す */
-	// s := models.NewSignalEvents()
-	// df, _ := models.GetAllCandle("BTC_JPY", time.Minute, 10)
-	// c1 := df.Candles[len(df.Candles)-2]
-	// c2 := df.Candles[len(df.Candles)-1]
-	// s.Buy("BTC_JPY", c1.Time.UTC(), c1.Close, 1.0, true)
-	// s.Sell("BTC_JPY", c2.Time.UTC(), c2.Close, 1.0, true)
-	// fmt.Println(models.GetSignalEventsByCount(1))
-	// fmt.Println(models.GetSignalEventsAfterTime(c1.Time))
-	// fmt.Println(s.CollectAfter(time.Now().UTC()))
-	// fmt.Println(s.CollectAfter(c1.Time))
 }
+
+// func main() {
+
+/* ログの設定 */
+// utils.LoggingSettings(config.Config.LogFile)
+// apiClient := bitflyer.New(config.Config.ApiKey, config.Config.ApiSecret)
+
+/* リアルタイムで情報を取得 */
+// tickerChannel := make(chan bitflyer.Ticker)
+// go apiClient.GetRealTimeTicker(config.Config.ProductCode, tickerChannel)
+// for ticker := range tickerChannel {
+// 	fmt.Println(ticker)
+// 	fmt.Println(ticker.GetMidPrice())
+// 	fmt.Println(ticker.DateTime())
+// 	fmt.Println(ticker.TruncateDateTime(time.Second))
+// 	fmt.Println(ticker.TruncateDateTime(time.Minute))
+// 	fmt.Println(ticker.TruncateDateTime(time.Hour))
+// }
+
+/* オーダー情報 */
+// order := &bitflyer.Order{
+// 	ProductCode:     config.Config.ProductCode,
+// 	ChildOrderType:  "LIMIT", // or MARKET
+// 	Side:            "BUY",   // or SELL
+// 	Price:           7000,
+// 	Size:            0.01,
+// 	MinuteToExpires: 1,
+// 	TimeInForce:     "GTC", // キャンセルするまで有効
+// }
+
+/* オーダーを出す */
+// res, _ := apiClient.SendOrder(order)
+// fmt.Println(res.ChildOrderAcceptanceID)
+
+/* オーダー情報をリストをみる */
+// i := "JRF20181012-144016-140584"
+// params := map[string]string{
+//	"product_code": config.Config.ProductCode,
+//	"child_order_acceptance_id": i,
+// }
+// r, _ := apiClient.ListOrder(params)
+// fmt.Println(r)
+
+// テーブルを出力する
+// fmt.Println(models.DbConnection)
+
+/* リアルタイムにBitFlyerからデータを取ってきてデータベースに保存する */
+// controllers.StreamIngestionData()
+// log.Println(controllers.StartWebServer())
+
+/* BUYとSELLのシグナルを出す */
+// s := models.NewSignalEvents()
+// df, _ := models.GetAllCandle("BTC_JPY", time.Minute, 10)
+// c1 := df.Candles[len(df.Candles)-2]
+// c2 := df.Candles[len(df.Candles)-1]
+// s.Buy("BTC_JPY", c1.Time.UTC(), c1.Close, 1.0, true)
+// s.Sell("BTC_JPY", c2.Time.UTC(), c2.Close, 1.0, true)
+// fmt.Println(models.GetSignalEventsByCount(1))
+// fmt.Println(models.GetSignalEventsAfterTime(c1.Time))
+// fmt.Println(s.CollectAfter(time.Now().UTC()))
+// fmt.Println(s.CollectAfter(c1.Time))
+// }
